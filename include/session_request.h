cf21cfcfd5fd9c6a2f805b1955c01a64e7ca0ed4
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-14 12:56:28 +0300

    Add tariff id to paid session request

--- include/session_request.h
+++ include/session_request.h
@@ -7,6 +7,7 @@ struct SessionRequest {
   String deviceId;
   String operationId;
   String sessionId;
+  String tariffId;
   uint8_t relayChannel = 0;
   uint32_t durationSec = 0;
   uint32_t issuedAt = 0;
