c34622a06f9421f77086d61172b68c7b9d181ec0
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-14 12:57:49 +0300

    Disable test payments by default

--- server/config/qroplate.php
+++ server/config/qroplate.php
@@ -4,7 +4,7 @@ return [
     'super_admin_login' => env('SUPER_ADMIN_LOGIN', 'superadmin'),
     'super_admin_password' => env('SUPER_ADMIN_PASSWORD', ''),
     'device_token_secret' => env('DEVICE_TOKEN_SECRET', ''),
-    'payment_provider' => env('PAYMENT_PROVIDER', 'test'),
+    'payment_provider' => env('PAYMENT_PROVIDER', 'disabled'),
     'reservation_minutes' => (int) env('RESERVATION_MINUTES', 2),
     'session_command_ttl_seconds' => (int) env('SESSION_COMMAND_TTL_SECONDS', 120),
 ];
