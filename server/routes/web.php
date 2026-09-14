ec42ab66f65acb0605dcb4fa2d127092aabbfa1f
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-14 10:56:14 +0300

    Protect admin routes and add login/device creation

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
 use App\Http\Controllers\Admin\DeviceController;
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\AdminSessionAuth;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::redirect('/', '/admin/devices');
 
-Route::prefix('admin')->name('admin.')->group(function () {
+Route::middleware(AdminSessionAuth::class)->prefix('admin')->name('admin.')->group(function () {
     Route::get('/devices', [DeviceController::class, 'index'])->name('devices.index');
+    Route::get('/devices/create', [DeviceController::class, 'create'])->name('devices.create');
+    Route::post('/devices', [DeviceController::class, 'store'])->name('devices.store');
     Route::get('/devices/{device}/edit', [DeviceController::class, 'edit'])->name('devices.edit');
     Route::put('/devices/{device}', [DeviceController::class, 'update'])->name('devices.update');
     Route::put('/devices/{device}/channels/{channel}', [DeviceController::class, 'updateChannel'])->name('devices.channels.update');
