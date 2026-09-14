d52b1d577662b92fe8c4f1a15118640744d15339
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-14 12:52:56 +0300

    Add payment and session token configuration

--- server/config/qroplate.php
+++ server/config/qroplate.php
@@ -3,4 +3,8 @@
 return [
     'super_admin_login' => env('SUPER_ADMIN_LOGIN', 'superadmin'),
     'super_admin_password' => env('SUPER_ADMIN_PASSWORD', ''),
+    'device_token_secret' => env('DEVICE_TOKEN_SECRET', ''),
+    'payment_provider' => env('PAYMENT_PROVIDER', 'test'),
+    'reservation_minutes' => (int) env('RESERVATION_MINUTES', 2),
+    'session_command_ttl_seconds' => (int) env('SESSION_COMMAND_TTL_SECONDS', 120),
 ];
